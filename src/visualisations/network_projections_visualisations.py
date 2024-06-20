f48d0d3180ae31473b402fe9f8a3d11d1fe13be4
xm3van <xm3van@gmail.com>
2024-06-20 20:22:38 +0100

    minor viz for wallet projections

--- src/visualisations/network_projections_visualisations.py
+++ src/visualisations/network_projections_visualisations.py
@@ -7,7 +7,7 @@ import numpy as np
 from src.utilities.metrics_and_tests import jaccard_similarity
 
 
-def load_graphs_from_snapshots(df_snapshots, input_directory):
+def load_token_graphs_from_snapshots(df_snapshots, input_directory):
     """
     Load graphs from the given snapshots and store them in a dictionary.
 
@@ -32,8 +32,38 @@ def load_graphs_from_snapshots(df_snapshots, input_directory):
         if os.path.exists(file_path):
             graph = nx.read_graphml(file_path)
             graphs[snapshot] = graph
-        # else:
-            # print(f"Graph file for snapshot {snapshot} does not exist.")
+        else:
+            print(f"Graph file for snapshot {snapshot} does not exist.")
+    
+    return graphs, dates
+
+def load_wallet_graphs_from_snapshots(df_snapshots, input_directory):
+    """
+    Load graphs from the given snapshots and store them in a dictionary.
+
+    Parameters:
+    df_snapshots (pd.DataFrame): DataFrame containing snapshot information with 'Block Height' and 'Date' columns.
+    path (str): Base directory path where the graph files are located.
+
+    Returns:
+    dict: A dictionary containing loaded graphs with snapshot as the key.
+    dict: A dictionary containing dates with snapshot as the key.
+    """
+    
+    graphs = {}
+    dates = {}
+    
+    for index, row in df_snapshots.iterrows():
+        snapshot = row['Block Height']
+        date = row['Date'].strftime('%Y-%m-%d')  # Format date as string
+        dates[snapshot] = date
+        file_path = os.path.join(input_directory, f"validated_wallet_projection_graph_{snapshot}.graphml")
+        
+        if os.path.exists(file_path):
+            graph = nx.read_graphml(file_path)
+            graphs[snapshot] = graph
+        else:
+            print(f"Graph file for snapshot {snapshot} does not exist.")
     
     return graphs, dates
 
@@ -96,6 +126,42 @@ def visualize_network_grid(graphs, dates, address_to_symbol, output_directory, l
     # Save the figure
     fig.suptitle('Validated Token Network Projections Over Time', fontsize=16)
     plt.savefig(os.path.join(output_directory, "network_projection_grid.png"), format='png', dpi=300)
+    plt.show()
+
+
+def visualize_wallet_network_grid(graphs, dates, address_to_symbol, output_directory, layout=nx.kamada_kawai_layout):
+    # Define the grid size
+    rows, cols = 3, 6  # Adjust based on the number of graphs
+    fig, axes = plt.subplots(rows, cols, figsize=(20, 10))
+    axes = axes.flatten()  # Flatten to iterate easily
+    
+    for ax, (snapshot, graph) in zip(axes, graphs.items()):
+        # Relabel nodes with symbols using the mapping
+        relabeled_graph = nx.relabel_nodes(graph, address_to_symbol)
+        
+        # Compute layout
+        pos = layout(relabeled_graph)  
+        
+        # Draw the graph with relabeled nodes
+        nx.draw(relabeled_graph, pos, ax=ax, with_labels=False, node_size=50, node_color='skyblue', edge_color='gray', font_size=8)
+        ax.set_title(dates[snapshot], fontsize=10)
+        ax.set_axis_off()  # Hide axis for clarity
+        
+        # Draw a rectangle around the plot area
+        rect = patches.Rectangle((0, 0), 1, 1, linewidth=1, edgecolor='black', facecolor='none', transform=ax.transAxes, clip_on=False)
+        ax.add_patch(rect)
+
+    # Turn off any unused subplots
+    for i in range(len(graphs), len(axes)):
+        axes[i].set_axis_off()
+    
+    # Adjust layout to prevent overlap
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.9)  # Adjust the top spacing to accommodate title if necessary
+    
+    # Save the figure
+    fig.suptitle('Validated Wallet Projections Over Time', fontsize=16)
+    plt.savefig(os.path.join(output_directory, "wallet_projection_grid.png"), format='png', dpi=300)
     plt.show()
     
 
